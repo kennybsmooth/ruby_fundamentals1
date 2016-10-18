56727c0e7e8766b4f2e59636764296b3b2fb720f
Kenny Bui <kennybui@Kennys-MacBook-Air.local>
2016-10-18 13:54:32 -0400

    Exercise 2 Complete

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#Good tip on $55 meal?
+#Tip 15%, so 55 x 0.15%
+#Should be 8.25
+puts 55*0.15
+#Can't add string and integer
+#Convert i to string using ".to_s"
+puts "You're #" + 1.to_s
+#output result by using #(x+y) inline
+puts "#{45628*7839} is the number we are looking for"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#The expression is true.
