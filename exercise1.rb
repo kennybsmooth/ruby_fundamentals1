16e8e93ddc12afa08d7e36a35a83f873c504a224
Kenny Bui <kennybui@Kennys-MacBook-Air.local>
2016-10-18 13:05:34 -0400

    Exercise 1

---
+++ exercise1.rb
@@ -0,0 +1,7 @@
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+puts "Hello\t\tworld"
+puts "1. Hello\n2. World"
+puts "Hello\b\b\b\b\Goodbye world"
+puts "Hello\rStart over world"
+
+:bitmaker
