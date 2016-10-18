de35c3023a40f37b0fde5d15eaecfe58b37b4692
Kenny Bui <kennybui@Kennys-MacBook-Air.local>
2016-10-18 15:44:42 -0400

    Exercise 4 Complete

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+one_to_hundred = (1..100)
+one_to_hundred.each do |num|
+if num % 3 == 0 and num % 5 == 0
+  puts "Bitmaker"
+elsif num % 3 == 0
+  puts "Bit"
+elsif num % 5 == 0
+  puts "Maker"
+else
+  puts num
+end
+end
